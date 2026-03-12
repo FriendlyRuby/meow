c4cb9f050c4528c7d9e87d4621b4e1d53b28c15f
FriendlyRuby <143893224+FriendlyRuby@users.noreply.github.com>
2026-03-12 22:43:07 +0300

    Добавил точку входа

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello World")
+from aiogram import Bot, Dispatcher
+from aiogram.utils import executor
+from config import TOKEN
+from bot.handlers import register_handlers
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+register_handlers(dp)
+
+if __name__ == "__main__":
+    executor.start_polling(dp)

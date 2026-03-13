7e2d2a37939c7b4bc120a65c6111649bbf016727
FriendlyRuby <143893224+FriendlyRuby@users.noreply.github.com>
2026-03-13 22:49:52 +0300

    random update

--- bot/handlers/random_anime.py
+++ bot/handlers/random_anime.py
@@ -1,7 +1,9 @@
+import requests
+import random
+
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
-import requests
 
 from bot.handlers.anime import send_anime_card
 
@@ -11,10 +13,28 @@ router = Router()
 @router.message(Command("random"))
 async def random_anime(message: Message):
 
-    url = "https://api.jikan.moe/v4/random/anime"
+    try:
 
-    r = requests.get(url).json()
+        # случайная страница топа
+        page = random.randint(1, 20)
 
-    anime = r["data"]
+        r = requests.get(
+            f"https://api.jikan.moe/v4/top/anime?page={page}"
+        ).json()
 
-    await send_anime_card(message, anime)
+        anime_list = r["data"]
+
+        # случайное аниме из топа
+        anime_short = random.choice(anime_list)
+
+        mal_id = anime_short["mal_id"]
+
+        # получаем полную инфу
+        anime = requests.get(
+            f"https://api.jikan.moe/v4/anime/{mal_id}"
+        ).json()["data"]
+
+        await send_anime_card(message, anime)
+
+    except Exception:
+        await message.answer("😿 Ошибка получения аниме, попробуй ещё раз.")

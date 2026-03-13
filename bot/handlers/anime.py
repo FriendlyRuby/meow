67e2c24e4db8616b384f57dc0c87fb840df5a797
FriendlyRuby <143893224+FriendlyRuby@users.noreply.github.com>
2026-03-13 21:27:09 +0300

    buttonsv2

--- bot/handlers/anime.py
+++ bot/handlers/anime.py
@@ -4,6 +4,7 @@ from aiogram.filters import Command
 import requests
 
 from deep_translator import GoogleTranslator
+from bot.keyboards.anime_buttons import anime_keyboard
 
 router = Router()
 
@@ -31,21 +32,33 @@ async def search_anime(message: Message):
     episodes = anime["episodes"]
     synopsis = anime["synopsis"]
     image = anime["images"]["jpg"]["image_url"]
+    mal_url = anime["url"]
 
-    # перевод описания
+    # трейлер
+    trailer = anime["trailer"]["url"]
+
+    # перевод
     try:
         synopsis_ru = GoogleTranslator(source="auto", target="ru").translate(synopsis)
     except:
         synopsis_ru = synopsis
 
+    synopsis_ru = synopsis_ru[:1500]
+
+    keyboard = anime_keyboard(trailer, mal_url)
+
     short_text = (
-    f"🎬 {title}\n\n"
-    f"⭐ Рейтинг: {score}\n"
-    f"📺 Эпизоды: {episodes}"
+        f"🎬 {title}\n\n"
+        f"⭐ Рейтинг: {score}\n"
+        f"📺 Эпизоды: {episodes}"
     )
 
-    await message.answer_photo(image, caption=short_text)
+    await message.answer_photo(
+        image,
+        caption=short_text,
+        reply_markup=keyboard
+    )
 
     await message.answer(
-    f"📖 Сюжет:\n\n{synopsis_ru}"
+        f"📖 Сюжет:\n\n{synopsis_ru}"
     )
